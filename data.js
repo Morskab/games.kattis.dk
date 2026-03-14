f9b552581da85dcfb66dd14ce034d22165f9000d
Anders Bruun Severinsen <202204885@post.au.dk>
2026-03-14 20:14:36 +0100

    Add the first 3 games played

--- data.js
+++ data.js
@@ -1,1 +1,41 @@
-const games = [];
+const games = [
+    {
+        date: "2026-02-16",
+        name: "Slaget om Aarhus",
+        players: "2-6",
+        playtime: "60-120 min.",
+        age: "14+",
+        price: "549,95",
+        rulebook: 5,
+        replayability: 5,
+        fun: 5,
+        strategy: 5,
+        interaction: 5
+    },
+    {
+        date: "2026-02-16",
+        name: "EGO: Rød",
+        players: "2-6",
+        playtime: "30",
+        age: "15+",
+        price: "259,95",
+        rulebook: 5,
+        replayability: 5,
+        fun: 5,
+        strategy: 5,
+        interaction: 5
+    },
+    {
+        date: "2026-03-09",
+        name: "Azul: Queens Garden",
+        players: "2-4",
+        playtime: "45-60 min.",
+        age: "10+",
+        price: "339,95",
+        rulebook: 5,
+        replayability: 5,
+        fun: 5,
+        strategy: 5,
+        interaction: 5
+    }
+];
